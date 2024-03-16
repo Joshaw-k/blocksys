7e8640eb8c051c3c2fcb1a3bfe8513decdcd3e4c
Joshaw-k <awinrin4bless@gmail.com>
2024-03-16 04:22:46 +0100

    first commit

--- src/utils/bitPrices.js
+++ src/utils/bitPrices.js
@@ -1,59 +1,59 @@
-import Binance from '../assets/Binance.png'
-import Bitcoin from '../assets/Bitcoin.png'
-import Tron from '../assets/Tron.png'
-import XRP from '../assets/XRP.png'
-import Tether from '../assets/Tether.png'
-import Litecoin from '../assets/Litecoin.png'
-import Ethereum from '../assets/Ethereum.png'
+import Binance from "../assets/Binance.png";
+import Bitcoin from "../assets/Bitcoin.png";
+import Tron from "../assets/Tron.png";
+import XRP from "../assets/XRP.png";
+import Tether from "../assets/Tether.png";
+import Litecoin from "../assets/Litecoin.png";
+import Ethereum from "../assets/Ethereum.png";
 
 export const bitPrices = [
   {
     logo: Bitcoin,
-    fullName: 'Bitcoin',
-    shortName: 'BTC',
-    price: '$29,433.00',
-    percent: 1.53,
+    fullName: "Bitcoin",
+    shortName: "BTC",
+    price: "$69,360.94",
+    percent: 1.4,
   },
   {
     logo: Ethereum,
-    fullName: 'Ethereum',
-    shortName: 'ETH',
-    price: '$1,905.52',
-    percent: -0.49,
+    fullName: "Ethereum",
+    shortName: "ETH",
+    price: "$3,747.63",
+    percent: -4.8,
   },
   {
     logo: Tether,
-    fullName: 'Tether',
-    shortName: 'USDT',
-    price: '$1.002',
-    percent: -0.81,
+    fullName: "Tether",
+    shortName: "USDT",
+    price: "$1.00",
+    percent: -0.1,
   },
   {
     logo: Binance,
-    fullName: 'BinanceCoin',
-    shortName: 'BNB',
-    price: '$327.36',
-    percent: -2.2,
+    fullName: "BinanceCoin",
+    shortName: "BNB",
+    price: "$618.10",
+    percent: 26.8,
   },
   {
     logo: XRP,
-    fullName: 'XRP',
-    shortName: 'XRP',
-    price: '$0.465',
-    percent: 0.81,
+    fullName: "XRP",
+    shortName: "XRP",
+    price: "$0.6427",
+    percent: 2.1,
   },
   {
     logo: Litecoin,
-    fullName: 'Litecoin',
-    shortName: 'LTC',
-    price: '$89.32',
-    percent: 0.21,
+    fullName: "Litecoin",
+    shortName: "LTC",
+    price: "$89.79",
+    percent: 0.6,
   },
   {
     logo: Tron,
-    fullName: 'Tron',
-    shortName: 'TRX',
-    price: '$0.0663',
-    percent: 1.28,
+    fullName: "Tron",
+    shortName: "TRX",
+    price: "$0.127",
+    percent: -7.6,
   },
-]
+];
