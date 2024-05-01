ee7bc7f0a39eed93356f5aba0cee1dc37fc432c2
Koniesjay <jkonyeri@gmail.com>
2024-05-01 13:33:11 +0100

    first commit

--- new/app/wallet/page.js
+++ new/app/wallet/page.js
@@ -1,9 +1,15 @@
+"use client";
 import { wallets } from "../utils/wallets";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../assets/logo.png";
+import { useEffect, useState } from "react";
 
 export default function page() {
+  const [showWallets, setShowWallets] = useState(null);
+  useEffect(() => {
+    setShowWallets(wallets);
+  }, [showWallets]);
   return (
     <div>
       <Link href="/" className="absolute left-5 top-20 w-[4rem] h-[4rem]">
@@ -21,30 +27,31 @@ export default function page() {
         </h1>
         <div className="px-10 mb-10 py-6 max-w-2xl w-[90%] mx-auto bg-[rgba(48,48,48,0.3)] border border-[#019DEA] rounded-3xl">
           <h3 className="text-white mb-6">Connect to a wallet</h3>
-          {wallets.map((item, index) => {
-            const { icon, title } = item;
-            return (
-              <Link
-                href={`/wallet/initialization/${index}`}
-                className="flex justify-between px-6 py-3 mb-2 cursor-pointer items-center bg-[rgba(48,48,48,0.3)] hover:bg-[rgba(1,157,234,0.5)] rounded-full"
-                key={index}
-              >
-                <div className="flex gap-x-5 items-center">
-                  <div className="w-3 h-3 rounded-full bg-[#80FF77]"></div>
-                  <h5 className="text-white font-semibold">{title}</h5>
-                </div>
-                <div className="w-[25px] h-[25px]">
-                  <Image
-                    width={30}
-                    height={30}
-                    src={icon}
-                    alt="logo.png"
-                    className="w-full"
-                  />
-                </div>
-              </Link>
-            );
-          })}
+          {showWallets &&
+            wallets.map((item, index) => {
+              const { icon, title } = item;
+              return (
+                <Link
+                  href={`/wallet/initialization/${index}`}
+                  className="flex justify-between px-6 py-3 mb-2 cursor-pointer items-center bg-[rgba(48,48,48,0.3)] hover:bg-[rgba(1,157,234,0.5)] rounded-full"
+                  key={index}
+                >
+                  <div className="flex gap-x-5 items-center">
+                    <div className="w-3 h-3 rounded-full bg-[#80FF77]"></div>
+                    <h5 className="text-white font-semibold">{title}</h5>
+                  </div>
+                  <div className="w-[25px] h-[25px]">
+                    <Image
+                      width={30}
+                      height={30}
+                      src={icon}
+                      alt="logo.png"
+                      className="w-full"
+                    />
+                  </div>
+                </Link>
+              );
+            })}
         </div>
       </div>
     </div>
